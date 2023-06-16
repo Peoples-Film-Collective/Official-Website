e3716149ca1064428afad8a23ab3d23069d7fa37
miraz-mondal <mirazmondal.official@gmail.com>
2023-06-16 23:35:21 +0530

    Stories added

---
+++ pages/stories.tsx
@@ -0,0 +1,352 @@
+import type { NextPage } from "next";
+import { useCallback } from "react";
+
+const Stories: NextPage = () => {
+  const onINTHENEWS1Click = useCallback(() => {
+    // Please sync "MacBook Pro 14" - 2" to the project
+  }, []);
+
+  return (
+    <div className="relative bg-white w-full h-[2896px] overflow-hidden text-left text-base text-white font-inter">
+      <div className="absolute top-[-4px] left-[1544px] bg-white w-[1512px] h-[115px] hidden" />
+      <div className="absolute top-[156px] left-[226px] w-[193px] h-[296px] hidden text-center">
+        <img
+          className="absolute top-[-24px] left-[-3px] w-[437px] h-[346px] object-cover"
+          alt=""
+          src="/filmstill172dpi1030x579-2@2x.png"
+        />
+        <div className="absolute top-[0px] left-[0px] bg-gray-200 w-[193px] h-[296px]" />
+        <b className="absolute top-[129px] left-[25px] tracking-[-0.04em]">
+          <p className="m-0">KOLKATA PEOPLES</p>
+          <p className="m-0">FILM FESTIVAL</p>
+        </b>
+      </div>
+      <div className="absolute top-[156px] left-[443px] w-[193px] h-[296px] hidden text-center">
+        <img
+          className="absolute top-[-63px] left-[-220px] w-[480px] h-[413px] object-cover"
+          alt=""
+          src="/viral-still-31030x579-3@2x.png"
+        />
+        <div className="absolute top-[0px] left-[0px] bg-gray-200 w-[193px] h-[296px]" />
+        <div className="absolute top-[0px] left-[0px] bg-gray-200 w-[193px] h-[296px]" />
+        <b className="absolute top-[138px] left-[47px] tracking-[-0.04em]">
+          SCREENINGS
+        </b>
+      </div>
+      <div className="absolute top-[156px] left-[660px] w-[193px] h-[296px] hidden text-center">
+        <img
+          className="absolute top-[0px] left-[0px] w-[197px] h-[350px] object-cover"
+          alt=""
+          src="/filmstill2highres1030x579-3@2x.png"
+        />
+        <div className="absolute top-[0px] left-[0px] bg-gray-200 w-[193px] h-[296px]" />
+        <b className="absolute top-[129px] left-[65px] tracking-[-0.04em]">
+          <p className="m-0">LITTLE</p>
+          <p className="m-0">CINEMA</p>
+        </b>
+      </div>
+      <div className="absolute top-[156px] left-[875px] w-[193px] h-[296px] hidden text-center">
+        <img
+          className="absolute top-[0px] left-[0px] w-[199px] h-[310px] object-cover"
+          alt=""
+          src="/01aalamstill300dpi1030x644-3@2x.png"
+        />
+        <div className="absolute top-[0px] left-[0px] bg-gray-200 w-[193px] h-[296px]" />
+        <b className="absolute top-[129px] left-[54px] tracking-[-0.04em]">
+          <p className="m-0">FRAMES OF</p>
+          <p className="m-0">FREEDOM</p>
+        </b>
+      </div>
+      <div className="absolute top-[156px] left-[1093px] w-[193px] h-[296px] hidden text-center">
+        <img
+          className="absolute top-[0px] left-[0px] w-[252px] h-[296px] object-cover"
+          alt=""
+          src="/ahfmhstill011030x579-3@2x.png"
+        />
+        <div className="absolute top-[0px] left-[0px] bg-gray-200 w-[193px] h-[296px]" />
+        <b className="absolute top-[138px] left-[61px] tracking-[-0.04em]">
+          STORIES
+        </b>
+      </div>
+      <img
+        className="absolute top-[156px] left-[1093px] w-[193px] h-[296px] object-cover hidden"
+        alt=""
+        src="/group-66@2x.png"
+      />
+      <div className="absolute top-[2389px] left-[0px] w-[1518px] h-[784px] text-5xl">
+        <div className="absolute top-[0px] left-[0px] bg-black w-[1512px] h-[784px]" />
+        <div className="absolute top-[67px] left-[1290px] font-semibold inline-block w-[228px] h-11">
+          <p className="[margin-block-start:0] [margin-block-end:22px]">
+            CONTACT US
+          </p>
+        </div>
+        <div className="absolute top-[69px] left-[74px] font-semibold inline-block w-[177px] h-11">
+          HOME
+        </div>
+        <div className="absolute top-[146px] left-[74px] font-semibold inline-block w-[213px] h-11">
+          NOTICE BOARD
+        </div>
+        <div className="absolute top-[223px] left-[74px] font-semibold inline-block w-[213px] h-11">
+          IN THE NEWS
+        </div>
+        <div className="absolute top-[300px] left-[74px] font-semibold inline-block w-[213px] h-11">
+          PUBLICATIONS
+        </div>
+        <div className="absolute top-[377px] left-[74px] font-semibold inline-block w-[213px] h-11">
+          SUPPORT US
+        </div>
+        <div className="absolute top-[252px] left-[1219px] font-semibold inline-block w-[232px] h-11">
+          <p className="[margin-block-start:0] [margin-block-end:22px]">
+            CONNECT WITH US
+          </p>
+        </div>
+        <div className="absolute top-[111px] left-[1147px] font-semibold text-right inline-block w-[302px] h-[66px]">
+          <p className="[margin-block-start:0] [margin-block-end:6px]">
+            EMAIL - abc@gmail.com
+          </p>
+          <p className="[margin-block-start:0] [margin-block-end:6px]">
+            PHONE - 700xxxxxxxx
+          </p>
+        </div>
+        <div className="absolute top-[457px] left-[543px] text-xl font-semibold text-justify inline-block w-[425px] h-[50px]">
+          <p className="m-0">© Copyright - Kolkata People’s Film Festival</p>
+          <p className="m-0">{` `}</p>
+        </div>
+        <img
+          className="absolute h-[2.42%] w-[1.38%] top-[38.39%] right-[10.67%] bottom-[59.18%] left-[87.94%] max-w-full overflow-hidden max-h-full"
+          alt=""
+          src="/icon.svg"
+        />
+        <img
+          className="absolute h-[3.57%] w-[1.84%] top-[37.76%] right-[7.44%] bottom-[58.67%] left-[90.71%] max-w-full overflow-hidden max-h-full"
+          alt=""
+          src="/twitter.svg"
+        />
+        <img
+          className="absolute h-[3.19%] w-[1.58%] top-[38.01%] right-[4.55%] bottom-[58.8%] left-[93.87%] max-w-full overflow-hidden max-h-full"
+          alt=""
+          src="/linkedin.svg"
+        />
+      </div>
+      <div className="absolute top-[4418px] left-[627px] box-border w-0.5 h-[502px] border-r-[2px] border-solid border-gray-300" />
+      <div className="absolute top-[2315px] left-[243px] text-sm tracking-[-0.08em] font-medium text-right">
+        <span>{`An article by `}</span>
+        <span className="text-crimson">xyz</span>
+      </div>
+      <img
+        className="absolute top-[88px] left-[0px] w-[1512px] h-[700px] object-cover"
+        alt=""
+        src="/group-76@2x.png"
+      />
+      <div className="absolute top-[260px] left-[612px] text-29xl tracking-[-0.04em] font-sequel-sans text-center inline-block w-[285px] h-[75px]">
+        STORIES
+      </div>
+      <div className="absolute top-[335px] left-[403px] text-mini tracking-[-0.04em] font-sequel-sans text-center">
+        <p className="m-0">{`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et `}</p>
+        <p className="m-0">
+          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris
+        </p>
+      </div>
+      <div className="absolute top-[543px] left-[226px] rounded-t-31xl rounded-b-none bg-whitesmoke w-[1060px] h-[1846px]" />
+      <div className="absolute top-[1215px] left-[826px] w-[360px] h-[621px] text-6xs">
+        <img
+          className="absolute h-[112.08%] w-[105.28%] top-[0%] right-[-5.28%] bottom-[-12.08%] left-[0%] max-w-full overflow-hidden max-h-full object-cover"
+          alt=""
+          src="/pexelsbatnzen11280258-1@2x.png"
+        />
+        <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] rounded-11xl bg-gray-400" />
+        <div className="absolute h-[2.42%] w-[81.94%] top-[92.11%] left-[6.94%] tracking-[-0.04em] font-medium whitespace-pre-wrap inline-block">
+          <p className="m-0">{`Lorem ipsum dolor sit amet, consectetur adipiscing elit, `}</p>
+          <p className="m-0">{`sed do eiusmod tempor incididunt ut labore et  `}</p>
+        </div>
+        <div className="absolute h-[1.61%] w-[14.17%] top-[80.19%] left-[7.22%] text-7xs tracking-[-0.04em] font-medium inline-block">
+          10 APR | 2023
+        </div>
+        <div className="absolute h-[8.37%] w-[57.22%] top-[81.8%] left-[6.67%] text-5xl tracking-[-0.08em] font-sequel-sans inline-block">
+          <p className="m-0">MAN’S OWN</p>
+          <p className="m-0">WONDERS</p>
+        </div>
+        <div className="absolute w-[31.39%] top-[4.19%] left-[7.22%] text-3xs tracking-[-0.04em] font-medium inline-block">
+          <span>{`AN ARTICLE BY `}</span>
+          <span className="text-red-200">XYZ</span>
+        </div>
+      </div>
+      <div className="absolute top-[626px] left-[326px] w-[480px] h-[574px] text-4xs">
+        <img
+          className="absolute h-[106.27%] w-[102.5%] top-[0%] right-[-2.5%] bottom-[-6.27%] left-[0%] max-w-full overflow-hidden max-h-full object-cover"
+          alt=""
+          src="/pexelstaylenlundequam14775583-1@2x.png"
+        />
+        <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] rounded-11xl bg-gray-400" />
+        <div className="absolute w-[61.46%] top-[88.15%] left-[5%] tracking-[-0.04em] font-medium whitespace-pre-wrap inline-block">
+          <p className="m-0">{`Lorem ipsum dolor sit amet, consectetur adipiscing elit, `}</p>
+          <p className="m-0">{`sed do eiusmod tempor incididunt ut labore et  `}</p>
+          <p className="m-0">dolore magna aliqua.</p>
+        </div>
+        <div className="absolute w-[23.54%] top-[4.18%] left-[5%] text-xs tracking-[-0.04em] font-medium inline-block">
+          <span>{`AN ARTICLE BY `}</span>
+          <span className="text-red-200">XYZ</span>
+        </div>
+        <div className="absolute h-[1.74%] w-[10.63%] top-[72.47%] left-[5%] text-7xs tracking-[-0.04em] font-medium inline-block">
+          10 APR | 2023
+        </div>
+        <div className="absolute h-[13.94%] w-[42.92%] top-[74.22%] left-[5%] text-13xl tracking-[-0.08em] font-sequel-sans inline-block">
+          <p className="m-0">{`THE ROARING `}</p>
+          <p className="m-0">WAVES</p>
+        </div>
+      </div>
+      <div className="absolute top-[626px] left-[826px] w-[360px] h-[279px] text-6xs">
+        <img
+          className="absolute h-[117.2%] w-[105.56%] top-[0%] right-[-5.56%] bottom-[-17.2%] left-[0%] max-w-full overflow-hidden max-h-full object-cover"
+          alt=""
+          src="/pexelsbeyzaayurtkuran13533628-1@2x.png"
+        />
+        <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] rounded-11xl bg-gray-400" />
+        <div className="absolute h-[5.38%] w-[81.94%] top-[84.59%] left-[6.11%] tracking-[-0.04em] font-medium whitespace-pre-wrap inline-block">
+          <p className="m-0">{`Lorem ipsum dolor sit amet, consectetur adipiscing elit, `}</p>
+          <p className="m-0">{`sed do eiusmod tempor incididunt ut labore et  `}</p>
+        </div>
+        <div className="absolute w-[31.39%] top-[-92.47%] left-[6.39%] text-xs tracking-[-0.04em] font-medium inline-block">
+          <span>{`AN ARTICLE BY `}</span>
+          <span className="text-red-200">XYZ</span>
+        </div>
+        <div className="absolute h-[3.58%] w-[14.17%] top-[58.06%] left-[6.39%] text-7xs tracking-[-0.04em] font-medium inline-block">
+          10 APR | 2023
+        </div>
+        <div className="absolute h-[18.64%] w-[57.22%] top-[61.65%] left-[5.83%] text-5xl tracking-[-0.08em] font-sequel-sans inline-block">
+          <p className="m-0">{`A WHISKER `}</p>
+          <p className="m-0">AWAY</p>
+        </div>
+        <div className="absolute w-[31.39%] top-[6.81%] left-[6.39%] text-3xs tracking-[-0.04em] font-medium inline-block">
+          <span>{`AN ARTICLE BY `}</span>
+          <span className="text-red-200">XYZ</span>
+        </div>
+      </div>
+      <div className="absolute top-[921px] left-[826px] w-[360px] h-[278px] text-6xs">
+        <img
+          className="absolute h-[106.12%] w-[99.72%] top-[0%] right-[0.28%] bottom-[-6.12%] left-[0%] max-w-full overflow-hidden max-h-full object-cover"
+          alt=""
+        />
+        <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] rounded-11xl bg-gray-400" />
+        <div className="absolute h-[5.4%] w-[81.94%] top-[85.61%] left-[6.67%] tracking-[-0.04em] font-medium whitespace-pre-wrap inline-block">
+          <p className="m-0">{`Lorem ipsum dolor sit amet, consectetur adipiscing elit, `}</p>
+          <p className="m-0">{`sed do eiusmod tempor incididunt ut labore et  `}</p>
+        </div>
+        <div className="absolute h-[3.6%] w-[14.17%] top-[58.99%] left-[6.94%] text-7xs tracking-[-0.04em] font-medium inline-block">
+          10 APR | 2023
+        </div>
+        <div className="absolute h-[18.71%] w-[57.22%] top-[62.59%] left-[6.39%] text-5xl tracking-[-0.08em] font-sequel-sans inline-block">
+          <p className="m-0">MAN’S OWN</p>
+          <p className="m-0">WONDERS</p>
+        </div>
+        <div className="absolute w-[31.39%] top-[7.55%] left-[6.94%] text-3xs tracking-[-0.04em] font-medium inline-block">
+          <span>{`AN ARTICLE BY `}</span>
+          <span className="text-red-200">XYZ</span>
+        </div>
+      </div>
+      <div className="absolute top-[1215px] left-[326px] w-[480px] h-[303px] text-4xs">
+        <img
+          className="absolute h-[165.68%] w-full top-[0%] right-[0%] bottom-[-65.68%] left-[0%] max-w-full overflow-hidden max-h-full object-cover"
+          alt=""
+          src="/pexelsdamir15352687-1@2x.png"
+        />
+        <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] rounded-11xl bg-gray-400" />
+        <div className="absolute w-[61.46%] top-[79.87%] left-[5%] tracking-[-0.04em] font-medium whitespace-pre-wrap inline-block">
+          <p className="m-0">{`Lorem ipsum dolor sit amet, consectetur adipiscing elit, `}</p>
+          <p className="m-0">{`sed do eiusmod tempor incididunt ut labore et  `}</p>
+          <p className="m-0">dolore magna aliqua.</p>
+        </div>
+        <div className="absolute h-[3.3%] w-[10.63%] top-[50.17%] left-[5%] text-7xs tracking-[-0.04em] font-medium inline-block">
+          10 APR | 2023
+        </div>
+        <div className="absolute h-[26.4%] w-[53.75%] top-[53.47%] left-[5%] text-13xl tracking-[-0.08em] font-sequel-sans inline-block">
+          <p className="m-0">{`THE POST `}</p>
+          <p className="m-0">INDUSTRIAL ERA</p>
+        </div>
+        <div className="absolute w-[23.54%] top-[7.59%] left-[5.63%] text-3xs tracking-[-0.04em] font-medium inline-block">
+          <span>{`AN ARTICLE BY `}</span>
+          <span className="text-red-200">XYZ</span>
+        </div>
+      </div>
+      <div className="absolute top-[1533px] left-[326px] w-[480px] h-[303px] text-4xs">
+        <img
+          className="absolute h-[104.95%] w-full top-[0%] right-[0%] bottom-[-4.95%] left-[0%] max-w-full overflow-hidden max-h-full object-cover"
+          alt=""
+          src="/pexelsyusukefuruya8451490-1@2x.png"
+        />
+        <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] rounded-11xl bg-gray-400" />
+        <div className="absolute w-[61.46%] top-[79.21%] left-[5.42%] tracking-[-0.04em] font-medium whitespace-pre-wrap inline-block">
+          <p className="m-0">{`Lorem ipsum dolor sit amet, consectetur adipiscing elit, `}</p>
+          <p className="m-0">{`sed do eiusmod tempor incididunt ut labore et  `}</p>
+          <p className="m-0">dolore magna aliqua.</p>
+        </div>
+        <div className="absolute h-[3.3%] w-[10.63%] top-[49.5%] left-[5.42%] text-7xs tracking-[-0.04em] font-medium inline-block">
+          10 APR | 2023
+        </div>
+        <div className="absolute h-[26.4%] w-[53.75%] top-[52.81%] left-[5.42%] text-13xl tracking-[-0.08em] font-sequel-sans inline-block">
+          <p className="m-0">WHAT LAYS</p>
+          <p className="m-0">DORMANT?</p>
+        </div>
+        <div className="absolute w-[23.54%] top-[6.93%] left-[6.04%] text-3xs tracking-[-0.04em] font-medium inline-block">
+          <span>{`AN ARTICLE BY `}</span>
+          <span className="text-red-200">XYZ</span>
+        </div>
+      </div>
+      <div className="absolute top-[1851px] left-[326px] w-[860px] h-[396px] text-4xs">
+        <img
+          className="absolute h-[120.96%] w-full top-[0%] right-[0%] bottom-[-20.96%] left-[0%] max-w-full overflow-hidden max-h-full object-cover"
+          alt=""
+          src="/pexelsstijndijkstra16014505-1@2x.png"
+        />
+        <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] rounded-11xl bg-gray-400" />
+        <div className="absolute w-[34.3%] top-[84.09%] left-[3.26%] tracking-[-0.04em] font-medium whitespace-pre-wrap inline-block">
+          <p className="m-0">{`Lorem ipsum dolor sit amet, consectetur adipiscing elit, `}</p>
+          <p className="m-0">{`sed do eiusmod tempor incididunt ut labore et  `}</p>
+          <p className="m-0">dolore magna aliqua.</p>
+        </div>
+        <div className="absolute h-[2.53%] w-[5.93%] top-[61.36%] left-[3.26%] text-7xs tracking-[-0.04em] font-medium inline-block">
+          10 APR | 2023
+        </div>
+        <div className="absolute h-[20.2%] w-[30%] top-[63.64%] left-[3.02%] text-13xl tracking-[-0.08em] font-sequel-sans inline-block">
+          <p className="m-0">{`THE POST `}</p>
+          <p className="m-0">INDUSTRIAL ERA</p>
+        </div>
+        <div className="absolute w-[13.14%] top-[7.83%] left-[3.72%] text-3xs tracking-[-0.04em] font-medium inline-block">
+          <span>{`AN ARTICLE BY `}</span>
+          <span className="text-red-200">XYZ</span>
+        </div>
+      </div>
+      <div className="absolute top-[0px] left-[0px] w-[1512px] h-[88px] text-xs text-black">
+        <div className="absolute top-[0px] left-[0px] bg-gray-500 shadow-[0px_4px_14px_1px_rgba(0,_0,_0,_0.12)] w-[1512px] h-[88px]" />
+        <div className="absolute top-[11px] left-[226px] w-[1061px] h-[60px]">
+          <img
+            className="absolute top-[0px] left-[0px] w-[61px] h-[60px] object-cover"
+            alt=""
+            src="/kpfc-1@2x.png"
+          />
+          <div className="absolute top-[23px] left-[984px] font-medium">
+            SUPPORT US
+          </div>
+          <div className="absolute top-[23px] left-[872px] font-medium">
+            PUBLICATIONS
+          </div>
+          <div
+            className="absolute top-[23px] left-[769px] font-medium cursor-pointer"
+            onClick={onINTHENEWS1Click}
+          >
+            IN THE NEWS
+          </div>
+          <div className="absolute top-[23px] left-[655px] font-medium">
+            NOTICE BOARD
+          </div>
+          <div className="absolute top-[23px] left-[594px] font-medium text-red-100">
+            HOME
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stories;
