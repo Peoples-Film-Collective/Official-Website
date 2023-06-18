c9e45a250e47aee6606e94745b7f08a6e3c4b422
miraz-mondal <mirazmondal.official@gmail.com>
2023-06-18 23:55:34 +0530

    publications page added

---
+++ pages/publications.tsx
@@ -0,0 +1,238 @@
+import type { NextPage } from "next";
+import { useCallback } from "react";
+
+const Publications: NextPage = () => {
+  const onINTHENEWS1Click = useCallback(() => {
+  }, []);
+
+  return (
+    <div className="relative bg-whitesmoke-300 w-full h-[2735px] overflow-hidden text-left text-mini text-black font-inter">
+      <div className="absolute top-[1870px] left-[-258px] rounded-[50%] bg-skyblue [filter:blur(200px)] w-[841px] h-[841px] hidden" />
+      <div className="absolute top-[595px] left-[1212px] rounded-[50%] bg-mediumorchid [filter:blur(200px)] w-[841px] h-[841px] hidden" />
+      <div className="absolute top-[-329px] left-[-199px] rounded-[50%] bg-mediumslateblue [filter:blur(200px)] w-[841px] h-[841px] hidden" />
+      <div className="absolute top-[-4px] left-[1544px] bg-white w-[1512px] h-[115px] hidden" />
+      <div className="absolute top-[2228px] left-[0px] w-[1518px] h-[784px] text-5xl text-whitesmoke-100">
+        <div className="absolute top-[0px] left-[0px] bg-black w-[1512px] h-[784px]" />
+        <div className="absolute top-[67px] left-[1290px] font-semibold inline-block w-[228px] h-11">
+          <p className="[margin-block-start:0] [margin-block-end:22px]">
+            CONTACT US
+          </p>
+        </div>
+        <div className="absolute top-[69px] left-[74px] font-semibold inline-block w-[177px] h-11">
+          HOME
+        </div>
+        <div className="absolute top-[146px] left-[74px] font-semibold inline-block w-[213px] h-11">
+          NOTICE BOARD
+        </div>
+        <div className="absolute top-[223px] left-[74px] font-semibold inline-block w-[213px] h-11">
+          IN THE NEWS
+        </div>
+        <div className="absolute top-[300px] left-[74px] font-semibold inline-block w-[213px] h-11">
+          PUBLICATIONS
+        </div>
+        <div className="absolute top-[377px] left-[74px] font-semibold inline-block w-[213px] h-11">
+          SUPPORT US
+        </div>
+        <div className="absolute top-[252px] left-[1219px] font-semibold inline-block w-[232px] h-11">
+          <p className="[margin-block-start:0] [margin-block-end:22px]">
+            CONNECT WITH US
+          </p>
+        </div>
+        <div className="absolute top-[111px] left-[1147px] font-semibold text-right inline-block w-[302px] h-[66px]">
+          <p className="[margin-block-start:0] [margin-block-end:6px]">
+            EMAIL - abc@gmail.com
+          </p>
+          <p className="[margin-block-start:0] [margin-block-end:6px]">
+            PHONE - 700xxxxxxxx
+          </p>
+        </div>
+        <div className="absolute top-[457px] left-[543px] text-xl font-semibold text-justify inline-block w-[425px] h-[50px]">
+          <p className="m-0">© Copyright - Kolkata People’s Film Festival</p>
+          <p className="m-0">{` `}</p>
+        </div>
+      </div>
+      <div className="absolute top-[154px] left-[230px] w-[1057px] h-[437px]">
+        <img
+          className="absolute top-[0px] left-[0px] w-[415px] h-[437px]"
+          alt=""
+          src="/rectangle-93.svg"
+        />
+        <div className="absolute top-[74px] left-[477px] text-17xl tracking-[-0.02em] uppercase font-futura-bold inline-block w-[580px]">
+          A deep dive into our work as a team
+        </div>
+        <div className="absolute top-[32px] left-[478px] tracking-[-0.03em] uppercase font-medium">
+          Author’s name
+        </div>
+        <div className="absolute top-[382px] left-[478px] tracking-[-0.03em] uppercase font-medium text-crimson">
+          READ MORE
+        </div>
+        <div className="absolute top-[191px] left-[477px] text-gray-400">
+          18th May, 2023
+        </div>
+        <img
+          className="absolute top-[171.5px] left-[478px] w-[47px] h-0.5"
+          alt=""
+          src="/line-31.svg"
+        />
+        <div className="absolute top-[244px] left-[477px] text-base tracking-[-0.02em] font-medium text-gray-400 text-justify inline-block w-[576px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <img
+          className="absolute top-[383.64px] left-[575px] w-6 h-[14.73px]"
+          alt=""
+          src="/arrow-22.svg"
+        />
+      </div>
+      <div className="absolute top-[673px] left-[230px] w-[1057px] h-[437px]">
+        <img
+          className="absolute top-[0px] left-[0px] w-[415px] h-[437px]"
+          alt=""
+          src="/rectangle-93.svg"
+        />
+        <div className="absolute top-[74px] left-[477px] text-17xl tracking-[-0.02em] uppercase font-futura-bold inline-block w-[580px]">
+          A deep dive into our work as a team
+        </div>
+        <div className="absolute top-[32px] left-[478px] tracking-[-0.03em] uppercase font-medium">
+          Author’s name
+        </div>
+        <div className="absolute top-[382px] left-[478px] tracking-[-0.03em] uppercase font-medium text-crimson">
+          READ MORE
+        </div>
+        <div className="absolute top-[191px] left-[477px] text-gray-400">
+          18th May, 2023
+        </div>
+        <img
+          className="absolute top-[171.5px] left-[478px] w-[47px] h-0.5"
+          alt=""
+          src="/line-31.svg"
+        />
+        <div className="absolute top-[244px] left-[477px] text-base tracking-[-0.02em] font-medium text-gray-400 text-justify inline-block w-[576px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <img
+          className="absolute top-[383.64px] left-[575px] w-6 h-[14.73px]"
+          alt=""
+          src="/arrow-22.svg"
+        />
+      </div>
+      <div className="absolute top-[1192px] left-[230px] w-[1057px] h-[437px]">
+        <img
+          className="absolute top-[0px] left-[0px] w-[415px] h-[437px]"
+          alt=""
+          src="/rectangle-93.svg"
+        />
+        <div className="absolute top-[74px] left-[477px] text-17xl tracking-[-0.02em] uppercase font-futura-bold inline-block w-[580px]">
+          A deep dive into our work as a team
+        </div>
+        <div className="absolute top-[32px] left-[478px] tracking-[-0.03em] uppercase font-medium">
+          Author’s name
+        </div>
+        <div className="absolute top-[382px] left-[478px] tracking-[-0.03em] uppercase font-medium text-crimson">
+          READ MORE
+        </div>
+        <div className="absolute top-[191px] left-[477px] text-gray-400">
+          18th May, 2023
+        </div>
+        <img
+          className="absolute top-[171.5px] left-[478px] w-[47px] h-0.5"
+          alt=""
+          src="/line-31.svg"
+        />
+        <div className="absolute top-[244px] left-[477px] text-base tracking-[-0.02em] font-medium text-gray-400 text-justify inline-block w-[576px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <img
+          className="absolute top-[383.64px] left-[575px] w-6 h-[14.73px]"
+          alt=""
+          src="/arrow-22.svg"
+        />
+      </div>
+      <div className="absolute top-[1725px] left-[227px] w-[1057px] h-[437px]">
+        <img
+          className="absolute top-[0px] left-[0px] w-[415px] h-[437px]"
+          alt=""
+          src="/rectangle-93.svg"
+        />
+        <div className="absolute top-[74px] left-[477px] text-17xl tracking-[-0.02em] uppercase font-futura-bold inline-block w-[580px]">
+          A deep dive into our work as a team
+        </div>
+        <div className="absolute top-[32px] left-[478px] tracking-[-0.03em] uppercase font-medium">
+          Author’s name
+        </div>
+        <div className="absolute top-[382px] left-[478px] tracking-[-0.03em] uppercase font-medium text-crimson">
+          READ MORE
+        </div>
+        <div className="absolute top-[191px] left-[477px] text-gray-400">
+          18th May, 2023
+        </div>
+        <img
+          className="absolute top-[171.5px] left-[478px] w-[47px] h-0.5"
+          alt=""
+          src="/line-31.svg"
+        />
+        <div className="absolute top-[244px] left-[477px] text-base tracking-[-0.02em] font-medium text-gray-400 text-justify inline-block w-[576px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <img
+          className="absolute top-[383.64px] left-[575px] w-6 h-[14.73px]"
+          alt=""
+          src="/arrow-22.svg"
+        />
+      </div>
+      <div className="absolute top-[0px] left-[0px] w-[1512px] h-[88px] text-xs">
+        <div className="absolute top-[0px] left-[0px] bg-gray-500 shadow-[0px_4px_14px_1px_rgba(0,_0,_0,_0.12)] w-[1512px] h-[88px]" />
+        <div className="absolute top-[11px] left-[226px] w-[1061px] h-[60px]">
+          <img
+            className="absolute top-[0px] left-[0px] w-[61px] h-[60px] object-cover"
+            alt=""
+            src="/kpfc-1@2x.png"
+          />
+          <div className="absolute top-[23px] left-[984px] font-medium">
+            SUPPORT US
+          </div>
+          <div className="absolute top-[23px] left-[872px] font-medium">
+            PUBLICATIONS
+          </div>
+          <div
+            className="absolute top-[23px] left-[769px] font-medium cursor-pointer"
+            onClick={onINTHENEWS1Click}
+          >
+            IN THE NEWS
+          </div>
+          <div className="absolute top-[23px] left-[655px] font-medium">
+            NOTICE BOARD
+          </div>
+          <div className="absolute top-[23px] left-[594px] font-medium text-red-100">
+            HOME
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Publications;
